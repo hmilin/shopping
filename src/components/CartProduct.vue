708a6d5935d212ab24548b65e92927ee6ed52c2b
hmilin <hmilin@163.com>
2018-09-05 22:59:06 +0800

    购物车

---
+++ src/components/CartProduct.vue
@@ -0,0 +1,83 @@
+<template>
+    <div id="cart-product">
+      <router-link :to="{ name: 'product-page', query: { page: product.product_id }}">
+        <div id="container">
+          <div class="img-container">
+            <img :src="product.imageList[0].url" :alt="product.imageList[0].desc">
+          </div>
+          <div class="desc">
+            <p class="product_name">
+              {{product.desc}}
+            </p>
+            <span class="price">￥{{product.price}}</span>
+          </div>
+        </div>
+      </router-link>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "CartProduct",
+    props: {
+      product: Object
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  @import '../style/mixin';
+  #cart-product {
+    width: 100%;
+    box-sizing: border-box;
+    padding: 10px 10px 10px 30px;
+    a {
+      #container {
+        font-size: 0;
+        overflow: hidden;
+        .img-container {
+          float: left;
+          @include px2rem(height, 200);
+          @include px2rem(width, 200);
+          img {
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+          }
+        }
+        .desc {
+          @include px2rem(margin-left, 200);
+          @include px2rem(height, 200);
+          margin-right: 0;
+          font-size: 0.5rem;
+          padding: 5px;
+          margin-top: auto;
+          color: #444;
+          position: relative;
+          p {
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 2;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            text-align: left;
+          }
+          span {
+            position: absolute;
+            bottom: 0;
+            right: 0;
+            color: #d20620;
+          }
+        }
+        .img-container:after {
+          content: "";
+          display: block;
+          float: right;
+        }
+      }
+    }
+  }
+</style>
--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,8 +1,22 @@
 <template>
     <div id="shopping-cart">
       <div id="product-list">
-        <div class="product">
-          ppppp
+        <div class="product" v-for="(product, index) in productList" :key="index">
+          <input type="checkbox" :name="product.name" :value="product.product_id" @click="radioCheck" :checked="" >
+          <CartProduct :product="product" class="cart-product"></CartProduct>
+        </div>
+      </div>
+      <div id="cart-bottom">
+        <div class="checkbox" @click="checkAll">
+          <div class="cheackbox-in" :class="{ 'checkbox-active': allCheck }"></div>
+        </div>
+        <span>全选</span>
+        <div class="account right">
+          <button>结算</button>
+        </div>
+        <div class="delete right">
+          <i class="iconfont"></i>
+          <button>删除</button>
         </div>
       </div>
     </div>
@@ -10,12 +24,18 @@
 
 <script>
   import { getCartContent } from "../api/getCartContent";
+  import CartProduct from './CartProduct'
 
   export default {
     name: "ShoppingCart",
+    components: {
+      CartProduct
+    },
     data() {
       return {
-        productList: []
+        productList: [],
+        allCheck: false,
+        checked: []
       }
     },
     methods: {
@@ -29,7 +49,28 @@
             alert('未登录')
           }
         })
+      },
+      //全选
+      checkAll() {
+        if(this.allCheck === false) {
+          this.productList.forEach((item) => {
+            this.checked.push(item.product_id);
+          })
+          this.allCheck = true;
+        }else if(this.allCheck === true) {
+            this.checked = [];
+            this.allCheck = false;
+        }
+      },
+      //单选
+      radioCheck() {
+        this.checkBox.forEach((item) => {
+          if(item.checked === true) {
+            this.checked.push(item.key);
+          }
+        })
       }
+
     },
     mounted() {
       this.$store.dispatch('setTitle','购物车');
@@ -39,13 +80,81 @@
 </script>
 
 <style scoped lang="scss">
+  @import "../style/mixin";
   #shopping-cart {
     width: 100%;
     padding: 10px;
     box-sizing: border-box;
     #product-list {
-      background-color: rgba(255, 255, 255, 0.5);
+      background-color: #efefef;
       width: 100%;
+      .product {
+        position: relative;
+        margin-bottom: 10px;
+        background-color: rgba(255, 255, 255, 0.5);
+        input {
+          width: 20px;
+          height: 20px;
+          border-radius: 20px;
+          border: 1px solid #999;
+          position: absolute;
+          left: 2px;
+          top: 50%;
+          transform: translate(0, -50%);
+        }
+        .cart-product {
+          display: inline-block;
+        }
+      }
+    }
+    #cart-bottom {
+      overflow: auto;
+      width: 100%;
+      box-sizing: border-box;
+      padding: 10px;
+      background-color: rgba(255, 255, 255, 0.5);
+      position: relative;
+      .checkbox {
+        position: absolute;
+        float: left;
+        height: 15px;
+        width: 15px;
+        border: 4px solid #999;
+        border-radius: 15px;
+        top: 50%;
+        left: 15px;
+        transform: translate(0, -50%);
+        .cheackbox-in {
+          border: 2px solid #efefef;
+          height: 11px;
+          width: 11px;
+          border-radius: 12px;
+        }
+      }
+      .checkbox-active {
+        background-color: #86AA9F;
+      }
+      .right {
+        float: right;
+        height: 30px;
+        @include px2rem(width, 120);
+        background-color: #86AA9F;
+        border-radius: 3px;
+        button {
+          font-size: 0.5rem;
+          background-color: transparent;
+          border: none;
+          color: #333;
+        }
+      }
+      span {
+        color: #999;
+        float: left;
+        font-size: 0.5rem;
+        font-weight: bold;
+        margin-left: 30px;
+        line-height: 30px;
+      }
     }
   }
 </style>
